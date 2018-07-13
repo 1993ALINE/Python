b7d30561c8a9964042deed2b93fe9b1a571f3dd5
ATIQUR RAHMAN ALINE <33422279+1993ALINE@users.noreply.github.com>
2018-07-13 23:16:31 +0600

    Add files via upload

---
+++ ClassifierBuild.py
@@ -0,0 +1,13 @@
+import sklearn
+from sklearn.datasets import  load_boston
+data = load_boston()
+
+label_names=data["target_names"]
+labels = data['target']
+feature_names = data['feature_names']
+features = data['data']
+
+print(label_names)
+print(labels[0])
+print(feature_names[0])
+print(features[0])
